c41eab587e52902e513cf1a9c05aa8e068156a36
Gideon Farrell <gideon@gtf.io>
2021-07-25 12:42:09 +0100

    fix escaping in HTML template

--- src/Html.hs
+++ src/Html.hs
@@ -1,31 +1,37 @@
-{-# LANGUAGE QuasiQuotes, OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes, OverloadedStrings, TemplateHaskell #-}
 
--- *** THIS MODULE IS A WORK IN PROGRESS ***
--- TODO:
---  - [x] render tree to json and include in template
---  - [ ] incorporate application code from javascript modules
---  - [ ] write tests
-
-module Html (renderHtml) where
+module Html (makeHtml) where
 
 import Data.ByteString.Lazy.UTF8 (ByteString, toString)
 import Data.Aeson (encode)
 import Text.Hamlet (shamlet, Html)
 import Data.Text (Text)
 import Text.Blaze.Html.Renderer.String (renderHtml)
+import Text.Blaze.Html (preEscapedToMarkup)
 import TaskTree
+
+import Language.Haskell.TH
+import qualified Data.Text.Encoding as TE
+import qualified Data.ByteString
+import Data.FileEmbed (embedFile, makeRelativeToProject)
+
+jsAppData :: Data.ByteString.ByteString
+jsAppData = $(makeRelativeToProject "js/dist/main.js" >>= embedFile)
 
 template :: TaskTreeNode -> Html
 template tree = let treejson = toString . encode $ tree
-                    appscript = "" :: String
+                    appjs    = TE.decodeUtf8 jsAppData
                    in [shamlet|
 $doctype 5
 <html>
   <head>
     <title>Grasana: Graphical Asana Projects
     <style>svg.panning text { user-select: none; }
-    <script language="javascript">window.treejson = #{treejson}
+    <script language="javascript">window.treejson = #{preEscapedToMarkup treejson}
   <body>
     <div id="AppContainer">
-    <script language="javascript">#{appscript}
+    <script language="javascript">#{preEscapedToMarkup appjs}
 |]
+
+makeHtml :: TaskTreeNode -> String
+makeHtml = renderHtml . template
